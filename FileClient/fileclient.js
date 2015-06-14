a7c5e59503afd6091d766dc573543a6357ad0152
Edilson Gonzalez <edilson.gonza@unitec.edu>
2015-06-14 18:23:42 +0000

    Cambios en el cliente

--- FileClient/fileclient.js
+++ FileClient/fileclient.js
@@ -4,15 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
-
-var models = require('./models/models');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var chat = require('./routes/chat');
-
+var io = require('socket.io');
 var app = express();
+var http = require('http');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,11 +20,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'ssshhsss', resave: false, saveUninitialized: true }));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/chat', chat);
+// Socket client
+
+var socket = require('socket.io-client')('http://green-box-37-202764.use1.nitrousbox.com/');
+
+socket.on('connect', function() {
+  console.log('Conectado al master');
+});
+
+//Routes
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -63,5 +64,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
-module.exports = app;
+var port = process.env.PORT || 3000;
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port);
--- FileClient/sockets/base.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(io) {
-	var models = require('../models/models');	
-	io.sockets.on('connection', function(socket) {
-		
-	});
-}
